2b9bcaf388565dd12989312dc1b754f9a8ce87bc
Daniel Mecke <daniel.mecke@cunningsoft.de>
2015-12-12 11:29:37 +0100

    fixed click events in firefox

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ var game = new Game();
 
 game.run();
 
-document.addEventListener('mousemove', function() { mouse.updatePosition(event); });
-document.addEventListener('mousedown', function() { mouse.buttonDown(event); });
-document.addEventListener('mouseup', function() { mouse.buttonUp(event); });
-document.addEventListener('keydown', function() { keyboard.addKey(event.keyCode); });
-document.addEventListener('keyup', function() { keyboard.removeKey(event.keyCode); });
+document.addEventListener('mousemove', function(event) { mouse.updatePosition(event); });
+document.addEventListener('mousedown', function(event) { mouse.buttonDown(event); });
+document.addEventListener('mouseup', function(event) { mouse.buttonUp(event); });
+document.addEventListener('keydown', function(event) { keyboard.addKey(event.keyCode); });
+document.addEventListener('keyup', function(event) { keyboard.removeKey(event.keyCode); });
